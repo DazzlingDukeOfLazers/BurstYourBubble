1d0b64132fe8f187c994f33d16228a50c7df9534
Daniel Lindmark <107583191+danielallspice@users.noreply.github.com>
2023-07-02 23:56:24 -0500

    Add basic rotation

--- load_svgs.gd
+++ load_svgs.gd
@@ -9,6 +9,12 @@ var scales = [0.5, 1, 1.5, 2]
 # Colors to apply
 var colors = [Color.MEDIUM_VIOLET_RED, Color.DEEP_SKY_BLUE, Color.FOREST_GREEN, Color.YELLOW_GREEN]
 
+# Rotation rates to apply
+var rotation_rate = [1, 1, 2, 1]
+
+# Store the created TextureRects
+var texture_rects = []
+
 func _ready():
 	for i in range(scales.size()):
 		# Create a new TextureRect
@@ -32,3 +38,10 @@ func _ready():
 
 		# Position the TextureRect so they don't overlap
 		texture_rect.position = Vector2(i * svg_texture.get_width() * scales[i], 0)
+		
+		# Add the TextureRect to the array
+		texture_rects.append(texture_rect)
+
+func _process(delta):
+	for i in range(texture_rects.size()):
+		texture_rects[i].rotation += rotation_rate[i] * delta
