1e714fad6e070539beaa56398d9f0c4fbf6c04dd
Daniel Lindmark <107583191+danielallspice@users.noreply.github.com>
2023-07-03 00:08:47 -0500

    Add drag and drop of sprites

--- load_svgs.gd
+++ load_svgs.gd
@@ -15,18 +15,21 @@ var rotation_rate = [1, 1, 2, 1]
 # Starting positions to apply
 var starting_positions = [Vector2(50, 50), Vector2(150, 150), Vector2(250, 250), Vector2(350, 350)]
 
-# Store the created Sprites
+# Store the created Sprite2Ds
 var sprites = []
+
+# Store the currently dragged Sprite2D
+var dragged_sprite = null
 
 func _ready():
 	for i in range(scales.size()):
-		# Create a new Sprite
+		# Create a new Sprite2D
 		var sprite = Sprite2D.new()
 
 		# Load SVG as StreamTexture
 		var svg_texture = load(svg_path)
 
-		# Assign the texture to Sprite
+		# Assign the texture to Sprite2D
 		sprite.texture = svg_texture
 
 		# Apply scale
@@ -35,18 +38,30 @@ func _ready():
 		# Apply color
 		sprite.modulate = colors[i]
 
-		# Set the pivot_offset to the center of the Sprite
+		# Set the pivot_offset to the center of the Sprite2D
 		sprite.centered = true
 
-		# Add the Sprite as a child of the current node
+		# Add the Sprite2D as a child of the current node
 		add_child(sprite)
 
-		# Position the Sprite based on the starting_positions array
+		# Position the Sprite2D based on the starting_positions array
 		sprite.position = starting_positions[i]
 
-		# Add the Sprite to the array
+		# Add the Sprite2D to the array
 		sprites.append(sprite)
 
 func _process(delta):
 	for i in range(sprites.size()):
 		sprites[i].rotation += rotation_rate[i] * delta
+
+func _unhandled_input(event):
+	if event is InputEventMouseButton:
+		if event.is_pressed():
+			for sprite in sprites:
+				if sprite.get_rect().has_point(sprite.to_local(event.position)):
+					dragged_sprite = sprite
+					return
+		else:
+			dragged_sprite = null
+	elif event is InputEventMouseMotion and dragged_sprite:
+		dragged_sprite.position += event.relative
