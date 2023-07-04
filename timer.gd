270cb42e75fb14bfd29eca5259189c8bdbb5275e
Daniel Lindmark <107583191+danielallspice@users.noreply.github.com>
2023-07-04 10:55:48 -0500

    Fix animation / rand mob start bug

--- timer.gd
+++ timer.gd
@@ -5,14 +5,14 @@ extends Label
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	time_start = Time.get_ticks_msec()
-	self.text = "Y'all ready for chimps?"
+#	time_start = Time.get_ticks_msec()
+#	self.text = "Y'all ready for chimps?"
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	time_now = Time.get_ticks_msec()
-	var time_elapsed = time_now - time_start
-	self.text = str(time_elapsed/1000.0)
+#	time_now = Time.get_ticks_msec()
+#	var time_elapsed = time_now - time_start
+#	self.text = str(time_elapsed/1000.0)
 	pass
