3334589b91422e9bf3f41027e4cdd55774a6017b
Daniel Lindmark <107583191+danielallspice@users.noreply.github.com>
2023-07-03 20:17:10 -0500

    Add score

---
+++ Score.gd
@@ -0,0 +1,19 @@
+extends Label
+
+@onready var score = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	self.text = str(score)
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	self.text = str(score)
+	pass
+
+
+func _on_texture_button_button_down():
+	score += 1
+	pass # Replace with function body.
