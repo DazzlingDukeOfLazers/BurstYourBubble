ef8e37d819ec03f41ca28c08de4518ae8f5863a6
Daniel Lindmark <107583191+danielallspice@users.noreply.github.com>
2023-07-03 20:11:24 -0500

    Add timer

---
+++ timer.gd
@@ -0,0 +1,18 @@
+extends Label
+
+@onready var time_start = 0
+@onready var time_now = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	time_start = Time.get_ticks_msec()
+	self.text = "Y'all ready for chimps?"
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	time_now = Time.get_ticks_msec()
+	var time_elapsed = time_now - time_start
+	self.text = str(time_elapsed/1000.0)
+	pass
