884ebf5e804dd9c15c816fa42dad53467ee869f1
Daniel Lindmark <107583191+danielallspice@users.noreply.github.com>
2023-07-19 19:38:49 -0500

    Add script/scene filename parse step 1

--- things/scene-code.gd
+++ things/scene-code.gd
@@ -1,5 +1,5 @@
 extends RichTextLabel
-
+#@onready var my_list = getNode("../OtherFiles")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,6 +8,11 @@ func _ready():
 	
 	var file = FileAccess.open(this_scene.scene_file_path, FileAccess.READ)
 	text = file.get_as_text()
+	
+	var text_list = text.split(" ", false)
+	for text_str in text_list:
+		if text_str.contains("path"):
+			print("["+ text_str + "]")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -19,6 +24,7 @@ func _toggle_code_visibiliy():
 	# Toggle code window visibility
 	self.visible = !self.visible
 	get_node("../code_bg").visible = self.visible
+	get_node("../OtherFiles").visible = self.visible
 
 func _on_button_button_down():
 	_toggle_code_visibiliy()
