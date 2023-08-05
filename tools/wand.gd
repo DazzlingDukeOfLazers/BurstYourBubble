232d39eea82889da916516ef4caa9864b86b6827
Daniel Lindmark <107583191+danielallspice@users.noreply.github.com>
2023-08-04 21:41:24 -0500

    add target_body.queue_free()  StaticBody2D to wand

--- tools/wand.gd
+++ tools/wand.gd
@@ -1,25 +1,36 @@
+## wand.gd
+# simple magic wand tool
+# - add StaticBody2d
+
 extends Node2D
+
 
 @export var water_scene: PackedScene
 
-
+var target_body : StaticBody2D 
 signal hit
 
 var target : Vector2
 
+var wand_collide : bool = false
+
 func _ready():
 	$objectDetectSprite.visible = false
+	wand_collide = false
 	pass # Replace with function body.
 
 
 func use():
-	
-	if $charge_timer.is_stopped():
-		var new_thing = load("res://things/water.tscn").instantiate()
-		new_thing.position = get_node("..").position + target
-		get_node("../../world").add_child(new_thing)
-		$charge_timer.start()
-		
+	if (wand_collide == false):
+		if $charge_timer.is_stopped():
+			var new_thing = load("res://things/water.tscn").instantiate()
+			new_thing.position = get_node("..").position + target
+			get_node("../../world").add_child(new_thing)
+			$charge_timer.start()
+	else:
+		if $charge_timer.is_stopped():
+			target_body.queue_free()
+			$charge_timer.start()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -47,30 +58,15 @@ func start(pos):
 	$CollisionShape2D.set_deferred(&"disabled", false)
 
 
-
-func _on_area_2d_area_entered(area):
-	$objectDetectSprite.visibile = true
-	pass # Replace with function body.
-
-
-func _on_area_2d_area_exited(area):
-	$objectDetectSprite.visible = false
-	pass # Replace with function body.
-
-
-
-
-
-func _on_area_2d_area_shape_entered(area_rid, area, area_shape_index, local_shape_index):
-	$objectDetectSprite.visibile = true
-	pass # Replace with function body.
-
-
 func _on_area_2d_body_entered(body):
+	if body is StaticBody2D:
+		target_body = body
+	
 	$objectDetectSprite.visible = true
-	pass # Replace with function body.
+	wand_collide = true
 
 
-func _on_area_2d_body_exited(body):
+
+func _on_area_2d_body_exited(_body):
 	$objectDetectSprite.visible = false
-	pass # Replace with function body.
+	wand_collide = false
