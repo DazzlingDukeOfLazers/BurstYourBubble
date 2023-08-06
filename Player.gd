f8170f83dbde051689efe008970d5ba606fc42b0
Daniel Lindmark <107583191+danielallspice@users.noreply.github.com>
2023-08-05 22:50:14 -0500

    clean comments

--- Player.gd
+++ Player.gd
@@ -6,13 +6,11 @@ var coin_count:int = 0
 
 @export var speed = 400 # pixels / sec
 @onready var screen_size
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
 	screen_size = get_viewport_rect().size
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	velocity = Vector2.ZERO # The player's movement vector.
 	if Input.is_action_pressed("move_right"):
@@ -30,15 +28,11 @@ func _process(_delta):
 	else:
 		$AnimatedSprite2D.stop()
 		
-#	position += velocity * delta
-#	position.x = clamp(position.x, 0, screen_size.x)
-#	position.y = clamp(position.y, 0, screen_size.y)
 	move_and_slide()
 	
 	if velocity.x != 0:
 		$AnimatedSprite2D.animation = "walk"
 		$AnimatedSprite2D.flip_v = false
-		# See the note below about boolean assignment.
 		$AnimatedSprite2D.flip_h = -velocity.x < 0
 	elif velocity.y != 0:
 		$AnimatedSprite2D.animation = "walk" # TBD, replace with better "up" animation
@@ -51,28 +45,10 @@ func start(pos):
 	show()
 	$CollisionShape2D.set_deferred(&"disabled", false)
 
-
-#func _on_Player_body_entered(_body):
-#	hide()
-##	self.visible = false # Player disappears after being hit.
-#	hit.emit()
-#	# Must be deferred as we can't change physics properties on a physics callback.
-#	$CollisionShape2D.set_deferred(&"disabled", true)
-
-
-
-
 func _on_body_entered(_body):
-	
-	
-	if true:
-#	if get_colliding_bodies.is_in_group("mobs"):
-		
-		hide()
-	#	self.visible = false # Player disappears after being hit.
-		hit.emit()
-		# Must be deferred as we can't change physics properties on a physics callback.
-		$CollisionShape2D.set_deferred(&"disabled", true)
+	hide()
+	hit.emit()
+	$CollisionShape2D.set_deferred(&"disabled", true)
 
 
 
